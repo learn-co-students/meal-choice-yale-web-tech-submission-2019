0f436a827c9677dca963221745c586f199247a5b
crisp-panel-7711 <secharmot@davidson.edu>
2019-01-18 21:39:35 +0000

    Done.

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,2 +1,11 @@
 # Define a method meal_choice that returns the meal_choice that was
 # passed into it and defaults to meat.
+
+
+def meal_choice(meal = "meat")
+  print "#{meal}"
+  puts "\n"
+  "#{meal}"
+end
+
+#meal_choice
